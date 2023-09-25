e10858237bc1226fec060c0c9dcaad286c30a518
Yuliya Bayur <juliy14497@gmail.com>
2023-09-25 14:49:15 +0300

    Added validation for the function

--- task5/students/views.py
+++ task5/students/views.py
@@ -1,35 +1,43 @@
-from faker import Faker
-
+from django.http import HttpResponse
 from django.shortcuts import render
+from faker import Faker
 
 from .models import Student
 
 fake = Faker()
 
 
-def student_info_generate():
-    return Student.objects.create(first_name=fake.first_name(), last_name=fake.last_name(), birth_date=fake.date())
-
-
 def generate_one_student(request):
-    fake_student = student_info_generate()
-    get_fake_info = Student.objects.get(pk=fake_student.pk)
+    fake_student = Student.objects.create(first_name=fake.first_name(), last_name=fake.last_name(), birth_date=fake.date())
 
-    data = {
-        'data': get_fake_info,
-    }
+    if request.method == "GET":
+        get_fake_info = Student.objects.get(pk=fake_student.pk)
 
-    return render(request, 'students/one_student.html', data)
+        data = {
+            'data': get_fake_info,
+        }
+
+        return render(request, 'students/one_student.html', data)
 
 
 def generate_students(request):
     count = request.GET.get('count')
     get_fake_info = []
 
-    for i in range(int(count)):
-        fake_student = student_info_generate()
-        student = Student.objects.get(pk=fake_student.id)
-        get_fake_info.append(student)
+    try:
+        int(count)
+    except ValueError:
+        return HttpResponse("Введіть цiле число")
+
+    if 0 < int(count) <= 100:
+        for i in range(int(count)):
+            fake_student = Student.objects.create(first_name=fake.first_name(),
+                                                  last_name=fake.last_name(),
+                                                  birth_date=fake.date())
+            student = Student.objects.get(pk=fake_student.id)
+            get_fake_info.append(student)
+    else:
+        return HttpResponse("Введіть цiле позитивне число не більше 100")
 
     data = {
         'data': get_fake_info,
