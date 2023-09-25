adfbfc0669c60afd7d306584054445b2fa8d3c87
Yuliya Bayur <juliy14497@gmail.com>
2023-09-25 18:16:57 +0300

    Changed the func for generating students

--- task5/students/views.py
+++ task5/students/views.py
@@ -25,19 +25,22 @@ def generate_students(request):
     get_fake_info = []
 
     try:
-        int(count)
+        if count is None:
+            count = 10
+        else:
+            count = int(count)
     except ValueError:
-        return HttpResponse("Введіть цiле число")
+        return HttpResponse("<h1>Введіть цiле число</h1>")
 
-    if 0 < int(count) <= 100:
-        for i in range(int(count)):
-            fake_student = Student.objects.create(first_name=fake.first_name(),
-                                                  last_name=fake.last_name(),
-                                                  birth_date=fake.date())
-            student = Student.objects.get(pk=fake_student.id)
-            get_fake_info.append(student)
-    else:
-        return HttpResponse("Введіть цiле позитивне число не більше 100")
+    if 0 >= count or count > 100:
+        return HttpResponse("<h1>Введіть цiле позитивне число не більше 100</h1>")
+
+    for i in range(count):
+        fake_student = Student.objects.create(first_name=fake.first_name(),
+                                              last_name=fake.last_name(),
+                                              birth_date=fake.date())
+        student = Student.objects.get(pk=fake_student.id)
+        get_fake_info.append(student)
 
     data = {
         'data': get_fake_info,
