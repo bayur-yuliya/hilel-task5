e8ecacca83556f39c0f279ed98d39a2456f34945
Yuliya Bayur <juliy14497@gmail.com>
2023-10-13 21:50:32 +0300

    Change black

--- task5/students/middleware.py
+++ task5/students/middleware.py
@@ -3,14 +3,15 @@ import time
 
 
 def log_middleware(get_response):
-
     def middleware(request):
         start_time = time.time()
         response = get_response(request)
         execution_time = time.time() - start_time
 
-        with open('log_info.txt', 'a') as log:
-            log.write(f'{datetime.datetime.now()}, request.path: {request.path}, request.method: {request.method}, execution_time: {execution_time} \n')
+        with open("log_info.txt", "a") as log:
+            log.write(
+                f"{datetime.datetime.now()}, request.path: {request.path}, request.method: {request.method}, execution_time: {execution_time} \n"
+            )
 
         return response
 
